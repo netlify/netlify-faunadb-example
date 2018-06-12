736e79f6b36b085ce6befd6596ed7910fa9e579b
davidwells <david@inboundnow.com>
2018-06-12 16:37:00 -0700

    mobile all teh things

--- src/App.js
+++ src/App.js
@@ -189,12 +189,16 @@ class App extends Component {
       <div className="app">
         <header className="app-header">
           <div className="app-title-wrapper">
-            <img src={logo} className="app-logo" alt="logo" />
-            <div className='app-title-text'>
-              <h1 className="app-title">Netlify + Fauna DB</h1>
-              <p className="app-intro">
-                Using FaunaDB & Netlify functions
-              </p>
+            <div className="app-title-wrapper">
+              <div className='app-left-nav'>
+                <img src={logo} className="app-logo" alt="logo" />
+                <div className='app-title-text'>
+                  <h1 className="app-title">Netlify + Fauna DB</h1>
+                  <p className="app-intro">
+                    Using FaunaDB & Netlify functions
+                  </p>
+                </div>
+              </div>
             </div>
             <div className='deploy-button-wrapper'>
               <a
