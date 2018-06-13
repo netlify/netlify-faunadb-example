c348a515de9227de1dd507dc0540c6c0dbf8e78e
davidwells <david@inboundnow.com>
2018-06-13 14:24:47 -0700

    add API utils

---
+++ src/utils/api.js
@@ -0,0 +1,42 @@
+/* Api methods to call /functions */
+
+const create = (data) => {
+  console.log('run new create')
+  return fetch('/.netlify/functions/todos-create', {
+    body: JSON.stringify(data),
+    method: 'POST'
+  }).then(response => {
+    return response.json()
+  })
+}
+
+const readAll = () => {
+  return fetch('/.netlify/functions/todos-read-all').then((response) => {
+    console.log(response)
+    return response.json()
+  })
+}
+
+const update = (todoId, data) => {
+  return fetch(`/.netlify/functions/todos-update/${todoId}`, {
+    body: JSON.stringify(data),
+    method: 'POST'
+  }).then(response => {
+    return response.json()
+  })
+}
+
+const deleteTodo = (todoId) => {
+  return fetch(`/.netlify/functions/todos-delete/${todoId}`, {
+    method: 'POST',
+  }).then(response => {
+    return response.json()
+  })
+}
+
+export default {
+  create: create,
+  readAll: readAll,
+  update: update,
+  delete: deleteTodo
+}
