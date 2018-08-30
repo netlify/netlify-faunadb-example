a1fbcced32bc56e50f82a260ce24477f01ef1fbb
David Wells <hello@davidwells.io>
2018-08-30 10:20:30 -0700

    Update todos-create.js

--- functions/todos-create.js
+++ functions/todos-create.js
@@ -10,7 +10,7 @@ const client = new faunadb.Client({
 exports.handler = (event, context, callback) => {
   /* parse the string body into a useable JS object */
   const data = JSON.parse(event.body)
-  console.log("Function `todo-create` invoked", data)
+  console.log("Hello webinar. Function `todo-create` invoked", data)
   const todoItem = {
     data: data
   }
