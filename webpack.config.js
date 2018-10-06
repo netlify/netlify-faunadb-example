fe4b9e43e9bca3be9e9c052529fba9bec31bee44
Lambros Petrou <lambrospetrou@gmail.com>
2018-10-06 13:05:22 +0100

    Fix netlify-lambda build process due to webpack3 requirement

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var webpack = require("webpack");
+// This is needed until netlify-lambda upgrades to webpack4
+var webpack = require("./node_modules/netlify-lambda/node_modules/webpack");
 
 /* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
