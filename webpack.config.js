0684e2df7464819091c38bdfb18860af033ab56d
Lambros Petrou <lambrospetrou@gmail.com>
2018-10-12 20:11:35 +0100

    Upgrade netlify-lambda to 1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-// This is needed until netlify-lambda upgrades to webpack4
-var webpack = require("./node_modules/netlify-lambda/node_modules/webpack");
+var webpack = require("webpack");
 
 /* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
