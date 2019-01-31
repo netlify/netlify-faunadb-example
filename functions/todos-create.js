36e12d4a7945cb7831772331fe89d5c45f5499fa
David Wells <hello@davidwells.io>
2019-01-31 14:02:16 -0800

    Update todos-create.js

--- functions/todos-create.js
+++ functions/todos-create.js
@@ -3,7 +3,7 @@ import faunadb from 'faunadb' /* Import faunaDB sdk */
 /* configure faunaDB Client with our secret */
 const q = faunadb.query
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET
+  secret: process.env.FAUNADB_SERVER_SECRET
 })
 
 /* export our lambda function as named "handler" export */
