20188a7982b84ff80d1448302ea6badadb4438d5
David Wells <hello@davidwells.io>
2019-01-31 14:02:58 -0800

    Update todos-read-all.js

--- functions/todos-read-all.js
+++ functions/todos-read-all.js
@@ -2,7 +2,7 @@ import faunadb from 'faunadb'
 
 const q = faunadb.query
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET
+  secret: process.env.FAUNADB_SERVER_SECRET
 })
 
 exports.handler = (event, context, callback) => {
